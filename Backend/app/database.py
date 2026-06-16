05be65f69c4b307f132f5481bb069666ff6f1eed
manta100usd <evanhauzal@gmail.com>
2026-06-16 14:53:21 +0700

    supabes

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -2,8 +2,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Kita kunci langsung nilainya ke port Docker PostgreSQL lokal Anda
-DATABASE_URL = "postgresql://postgres:postgres@127.0.0.1:5434/kerjole_db"
+# Menggunakan URL Pooler Supabase Sydney dengan driver psycopg2 yang sudah terinstal di laptopmu
+DATABASE_URL = "postgresql+psycopg2://postgres:XnJPb4kGqIcy1G3B@db.ikglmnfjszgufesxafag.supabase.co:5432/postgres"
+SECRET_KEY = "kerjole-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
