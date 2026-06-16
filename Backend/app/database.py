516298192c1009802b3110816e995c6c44ed9a85
manta100usd <evanhauzal@gmail.com>
2026-06-16 11:53:34 +0700

    simpan

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -1,19 +1,13 @@
 import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()
-
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+psycopg://postgres:postgres@db:5432/kerjole_db"
-)
+# Kita kunci langsung nilainya ke port Docker PostgreSQL lokal Anda
+DATABASE_URL = "postgresql://postgres:postgres@127.0.0.1:5434/kerjole_db"
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
